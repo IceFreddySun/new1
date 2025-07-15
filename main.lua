1db054889eba98759d354f5ae2a0f42acfeaf9fb
IceFreddySun <superricefreddygd@gmail.com>
2025-07-15 10:14:18 +0300

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,2 @@
+local clone = workspace.mihk_fredeaxolotl["[Celestial, Windstruck, Shocked, Choc, Bloodlit, Paradisal, Zombified, HoneyGlazed, Frozen, Sandy, M"]:Clone()
+	clone.Parent = game.Players.LocalPlayer.Backpack
